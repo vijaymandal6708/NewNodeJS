e175d589a0b41582f0aff9030fec70447c2277f9
vijaymandal6708 <vijaymandal6708@gmail.com>
2026-02-07 18:32:13 +0530

    role based authentication

--- 02_Role_Based_Authentication_Practice/client/src/pages/AdminDashboard.jsx
+++ 02_Role_Based_Authentication_Practice/client/src/pages/AdminDashboard.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 const AdminDashboard = () => {
+
+  const loadData = async () => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+
+    const response = await axios.get(
+      "http://localhost:4000/user/user-dashboard-validate",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    console.log(response.data);
+}
+
   return (
     <>
       
