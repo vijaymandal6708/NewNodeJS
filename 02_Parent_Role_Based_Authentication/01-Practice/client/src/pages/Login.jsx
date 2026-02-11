1d48f9870d1351d9c87057124100dc8bd3fe87ad
vijaymandal6708 <vijaymandal6708@gmail.com>
2026-02-11 23:13:01 +0530

    role based authentication

--- 02_Parent_Role_Based_Authentication/01-Practice/client/src/pages/Login.jsx
+++ 02_Parent_Role_Based_Authentication/01-Practice/client/src/pages/Login.jsx
@@ -4,7 +4,10 @@ const Login = () => {
   return (
     <>
       <h1>this is the login page</h1>
-    </>
+
+      Enter email : <input type="email" name="email" onChange={handleInput} /> <br /> <br />
+      Enter password : <input type="password" name="password" onChange={handleInput} /> <br /> <br />
+     </>
   )
 }
 
