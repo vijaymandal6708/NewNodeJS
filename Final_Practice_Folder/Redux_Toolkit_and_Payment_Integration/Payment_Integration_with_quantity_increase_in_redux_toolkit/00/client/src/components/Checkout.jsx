88d51f1b80e513f85069c18c868fdc9876a6667b
vijaymandal6708 <vijaymandal6708@gmail.com>
2026-04-18 00:51:52 +0530

    payment integration

--- Final_Practice_Folder/Redux_Toolkit_and_Payment_Integration/Payment_Integration_with_quantity_increase_in_redux_toolkit/00/client/src/components/Checkout.jsx
+++ Final_Practice_Folder/Redux_Toolkit_and_Payment_Integration/Payment_Integration_with_quantity_increase_in_redux_toolkit/00/client/src/components/Checkout.jsx
@@ -1,65 +1,78 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Checkout = () => {
-    const cartItems = useSelector((state)=>state.cart);
+  const cartItems = useSelector((state) => state.cart);
 
-    const cartTotal = cartItems.reduce((sum,item)=> sum + item.price * item.quantity , 0);
+  const cartTotal = cartItems.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
 
-    const deliveryCharge = 99;
+  const deliveryCharge = 99;
 
-    const finalAmount = cartTotal+deliveryCharge;
+  const finalAmount = cartTotal + deliveryCharge;
 
-    const handlePayment =async()=>{
-        try {
-            const response = await axios.post("http://localhost:8000/create-order", {
-            amount: finalAmount
-            });
+  const handlePayment = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/create-order", {
+        amount: finalAmount,
+      });
 
-            const options = {
-                key: "",
-                amount: response.data.amount,
-                currency: "INR",
-                name: "GadgetGalaxy",
-                description: "Order Payment",
-                order_id: response.data.id,
-    
-                handler: function(response){
-                    alert("Payment Success");
-                    console.log(response);
-                }
-            };
-
-            const rzp = new window.Razorpay(options);
-            rzp.open();
-
-        } catch (error) {
-            console.error(error);
-            alert("Payment Failed"); 
-        }
+      const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY,
+        amount: response.data.amount,
+        currency: "INR",
+        name: "Gadget Galaxy",
+        description: "Payment",
+        order_id: response.data.id,
+        handler: function (response) {
+          alert("payment success");
+          console.log(response);
+        },
+      };
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    } catch (error) {
+        alert("payment failed");
+        console.log(error);
     }
-
+  };
   return (
-    <div style={{display:"flex",flexDirection:"column",padding:"30px 140px"}}>
-        <h2 style={{paddingLeft:"550px"}}>Checkout Page</h2>
-       {
-        cartItems.map((product,index)=>(
-            <div className="product" key={index} style={{height:"180px",width:"1200px",border:"1px solid black",padding:"10px 50px",boxSizing:"border-box"}}>
-                <p>name : {product.name}</p>
-                <p>price : {product.price}</p>
-                <p>quantity : {product.quantity}</p>
-                <p>total : {product.price*product.quantity}</p>
-            </div>
-        ))
-       }
-       <h4>cart total : {cartTotal}</h4>
-       <h4>delivery charge : {deliveryCharge}</h4>
-       <h4>finalAmount : {finalAmount}</h4>
-       <button onClick={handlePayment}>Proceed to Payment</button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "30px 140px",
+      }}
+    >
+      <h2 style={{ paddingLeft: "550px" }}>Checkout Page</h2>
+      {cartItems.map((product, index) => (
+        <div
+          className="product"
+          key={index}
+          style={{
+            height: "180px",
+            width: "1200px",
+            border: "1px solid black",
+            padding: "10px 50px",
+            boxSizing: "border-box",
+          }}
+        >
+          <p>name : {product.name}</p>
+          <p>price : {product.price}</p>
+          <p>quantity : {product.quantity}</p>
+          <p>total : {product.price * product.quantity}</p>
+        </div>
+      ))}
+      <h4>cart total : {cartTotal}</h4>
+      <h4>delivery charge : {deliveryCharge}</h4>
+      <h4>finalAmount : {finalAmount}</h4>
+      <button onClick={handlePayment}>Proceed to Payment</button>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
--- Final_Practice_Folder/Redux_Toolkit_and_Payment_Integration/Payment_Integration_with_quantity_increase_in_redux_toolkit/00/server/index.js
+++ Final_Practice_Folder/Redux_Toolkit_and_Payment_Integration/Payment_Integration_with_quantity_increase_in_redux_toolkit/00/server/index.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const Razorpay = require("razorpay");
 
 app.use(cors());
 app.use(express.json());
 
 const razorpay = new Razorpay({
-    key_id: "",
-    key_secret: ""
+    key_id:process.env.RAZORPAY_KEY,
+    key_secret:process.env.RAZORPAY_SECRET,
 });
 
-app.post("/create-order", async (req,res)=>{
-  const options = {
-    amount : req.body.amount * 100,
-    currency : "INR",
-    receipt : "order_rcptid_11"
-  };
+app.post("/create-order", async(req,res)=>{
+    const options = {
+        amount:req.body.amount*100,
+        currency:"INR",
+        receipt:"order_rcptid_11",
+    };
 
-  const order = await razorpay.orders.create(options);
-
-  res.json(order);
+    const order = await razorpay.orders.create(options);
+     
+    res.json(order);
 });
 
 app.listen(8000, ()=>{
-    console.log("server running on port 8000");
+    console.log("server is running on port 8000");
 })
