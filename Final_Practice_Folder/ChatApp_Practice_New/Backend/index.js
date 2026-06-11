d7f39cec1a1d9fe5e3fc88783cebdef1588d04af
vijaymandal6708 <vijaymandal6708@gmail.com>
2026-06-11 18:49:58 +0530

    chatapp practice

--- Final_Practice_Folder/ChatApp_Practice_New/Backend/index.js
+++ Final_Practice_Folder/ChatApp_Practice_New/Backend/index.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
+import User from "./models/user.model.js";
 
 dotenv.config();
 
@@ -29,6 +31,22 @@ try {
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
+// code for deployment
+
+if(process.env.NODE_ENV === 'production'){
+   const dirPath = path.resolve();
+   app.use(express.static("Frontend/dist"));
+   app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(dirPath, 'Frontend/dist','index.html'));
+   })
+}
+
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
+});
+
+const loggedInUserId = req.user._id;
+
+const filteredUsers = Users.find({
+    id:{ $ne: loggedInUserId }
 });
